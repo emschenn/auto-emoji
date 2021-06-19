5bed1b461ccca3c925f3ac5841397999f05a5ad1
emschenn <emilynov14@gmail.com>
2021-06-20 00:26:11 +0800

    fix timeout error on heroku

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,13 @@ from content_based import ContentBased
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-word2vec_path = './GoogleNews-vectors-negative300.bin'
-mapping_path = 'emoji_mapping.p'
-e2v_path = './emoji2vec.bin'
 content_based = ContentBased()
 
 
 @app.route('/')
 @cross_origin()
 def index():
-    return "This is the backend ou auto-emoji project 😉"
+    return "This is the backend of auto-emoji project 😉"
 
 
 @app.route('/predict', methods=['POST'])
@@ -30,5 +27,8 @@ def postPredict():
         'contentOption': content_option,
     })
 
-# if __name__ == '__main__':
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 #     app.run(host='0.0.0.0', port=3000, debug=True, use_reloader=False)
