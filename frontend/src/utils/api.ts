83f5a25dd4b8d1df974b150eccc51d44d38b4244
emschenn <emilynov14@gmail.com>
2021-06-20 23:33:02 +0800

    no-cors

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -5,12 +5,12 @@ export const postData = (url, data) => {
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     credentials: "same-origin", // include, same-origin, *omit
     headers: {
-      "Access-Control-Allow-Origin":"*",
+      "Access-Control-Allow-Origin": "*",
       "user-agent": "Mozilla/4.0 MDN Example",
       "content-type": "application/json",
     },
     method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, cors, *same-origin
+    mode: "no-cors", // no-cors, cors, *same-origin
     redirect: "follow", // manual, *follow, error
     referrer: "no-referrer", // *client, no-referrer
   }).then((response) => response.json()); // 輸出成 json
